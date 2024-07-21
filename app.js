477e9fcc0ca6821cf6ed3c9ad6f039e12d4012d7
kim <145192439+KimNattanan@users.noreply.github.com>
2024-07-21 23:33:26 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful:', registration);
+      })
+      .catch(error => {
+        console.error('ServiceWorker registration failed:', error);
+      });
+  });
+}
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+});
