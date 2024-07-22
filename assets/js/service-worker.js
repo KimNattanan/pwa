e0211c8755b32611afef8a0d6207d7ad7fb952e2
kim <145192439+KimNattanan@users.noreply.github.com>
2024-07-23 00:32:07 +0700

    Add files via upload

---
+++ assets/js/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '../../index.html',
+  '../js/main.js',
+  '../js/vender/pixi.min.js',
+  '../css/style.css',
+  '../img/bg.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
